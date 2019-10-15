202b9ec8603cf094904a5e4e149c0e238c011b91
Fusion86 <fusion86@cerbus.nl>
2019-10-15 20:56:03 +0200

    Add name and version

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,14 @@
 #include <rtos.hpp>
 
 #include "tasks.hpp"
+#include "version.h"
 
 int main() {
+    // Wait for the PC console to start
+    hwlib::wait_ms(2000);
+
+    hwlib::cout << "SparkySparky v" << SPARKYSPARKY_VERSION << "\n";
+
     auto t1 = pause_detector();
     auto t2 = msg_decoder();
     auto t3 = msg_logger();
