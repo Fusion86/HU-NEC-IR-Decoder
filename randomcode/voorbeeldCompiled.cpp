3af0b7ad53b95dae2a50699808a42d52a2c74d94
finnff <finn.fonteijn@student.hu.nl>
2019-10-17 12:02:39 +0200

    Werkt op hardware (5 meter) code nog niet

--- randomcode/voorbeeldCompiled.cpp
+++ randomcode/voorbeeldCompiled.cpp
@@ -1,67 +1,80 @@
-// simple IR signal detector
-
 #include "hwlib.hpp"
+#include <cstring>
 
 unsigned long nec_remote_read(hwlib::target::pin_in IRpin);
 
+int bytes[32] = {0};
 int main(void) {
 
   namespace target = hwlib::target;
 
   auto tsop_signal = target::pin_in(target::pins::d8);
-  auto tsop_gnd = target::pin_out(target::pins::d9);
-  auto tsop_vdd = target::pin_out(target::pins::d10);
-  tsop_gnd.write(0);
-  tsop_vdd.write(1);
-  tsop_gnd.flush();
-  tsop_vdd.flush();
 
   // auto led = target::pin_out(target::pins::led);
 
-
   for (;;) {
-    hwlib::cout << nec_remote_read(tsop_signal) << hwlib::endl;
+    for (unsigned int i = 0; i < 32; i++) {
+      bytes[i] = {0};
+    };  
+    nec_remote_read(tsop_signal);
+    for (unsigned int i = 0; i < 32; i++) {
+      hwlib::cout << bytes[i];
+      if (i == 7 || i == 15 || i == 23) {
+        hwlib::cout << ',';
+      }
+    }
+    hwlib::cout << '\n';
   }
 }
 
 unsigned long nec_remote_read(hwlib::target::pin_in IRpin) {
-  unsigned long ir_code;
-  unsigned long count = 0, i;
-  // Check 9ms pulse (remote control sends logic high)
-  while (IRpin.read() && (count < 180)) {   ///mischien  inverteren van pin.Read()? als niet werkt.
+  // unsigned long ir_code;
+  unsigned int count = 0;
+
+  while (IRpin.read() && (count < 300)) { /// 9MS ON
     count++;
     hwlib::wait_us(40);
   }
-
-  count = 0;
-  // Check 4.5ms space (remote control sends logic low)
-  while (IRpin.read() && (count < 90)) {
-    count++;
-    hwlib::wait_us(40);
-  }
-
-  // Read code message (32-bit)
-  for (i = 0; i < 32; i++) {
+  if (count > 180 || count < 240) { // 4.5MS OFF
     count = 0;
-    while ((!IRpin.read()) && (count < 15)) {
+
+    while (!IRpin.read() && (count < 200)) {
       count++;
       hwlib::wait_us(40);
     }
-    if ((count > 14) || (count < 2)) // NEC protocol?
-      return 0;
+    if (count > 80 || count < 120) {
+      for (unsigned int i = 0; i < 32; i++) {
+        count = 0;
+        while (IRpin.read() && (count < 800)) {
+          count++;
+          hwlib::wait_us(20);
+        }
 
-    count = 0;
-    while ( IRpin.read() && (count < 30)) {
-      count++;
-      hwlib::wait_us(40);
-    }
-    if ((count > 29) || (count < 4)) // NEC protocol?
-      return 0;
+        count = 0;
+        while (!IRpin.read() && (count < 110)) {
+          count++;
+          hwlib::wait_us(20);
+        }
+        // if (count > 95) {
+        //   return 0;
+        // }
+        // if (count < 6) {
+        //   return 0;
+        // }
 
-    if (count > 20)                  // If space width > 1ms
-      ir_code |= 1ul << (31 - i);    // Write 1 to bit (31 - i)
-    else                             // If space width < 1ms
-      ir_code &= ~(1ul << (31 - i)); // Write 0 to bit (31 - i)
-  }
-  return ir_code;
-}
+        if (count > 1) { // If space width > 1ms
+          bytes[i] = 1;
+          // hwlib::cout << "1" << hwlib::endl;
+
+        } else { // If space width < 1ms
+          bytes[i] = 0;
+          // hwlib::cout << "0" << hwlib::endl;
+        }
+      };
+
+      return 0;
+    };
+    return 0;
+  };
+  return 0;
+};
